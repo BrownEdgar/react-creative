e90f402c1325048e46f506beaec38c500a1daaac
Rubenners <rubennerseryan00@gmail.com>
2023-06-01 01:03:59 +0400

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import car from './Rectangle 5 (1).png'
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-     <h1>React creative</h1>
+    <div className="container">
+     <h1>Heading 1</h1>
+     <h2>Heading 2</h2>
+    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+    <img src={ car } alt="car"/>
     </div>
   );
 }
