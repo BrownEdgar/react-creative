5b01f256f2abff4ba6cb100c30fc1232ae49e19a
Khaninyan <davkhaninyan@gmail.com~>
2023-05-31 15:23:55 +0400

    r-8504 finished by Davit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.scss';
+import work_1 from "./Img/work_1.jpg"
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>React creative</h1>
+    <div className="container">
+     <h1>Work</h1>
+     <img src={work_1} alt="work_1" />
+     <div className="work-title">
+      <h2>Designing Dashboards</h2>
+      <div className='work-date'></div>
+      <p className='work-date-text'>2020</p>
+      <p className="work-dashboard" >Dashboard</p>
+      <p className="work-text">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+     </div>
+     <div className="vector"></div>
     </div>
   );
 }
