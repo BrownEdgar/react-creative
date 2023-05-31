4efc06368a864141582cdbe03c65621dd3b9b139
Mamikyan-G <garnikmamikyan9@gmail.com>
2023-06-01 00:23:45 +0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
-
+// import './App.css';
+import './App.scss';
 function App() {
   return (
     <div className="App">
-     <h1>React creative</h1>
+     <footer>
+			<div className='footer-img'>
+				<div className='footer-img-icon'>
+					<a href="https://ru-ru.facebook.com/"><img src={require('./img-footer/fb.png')} alt="" /></a>
+					<a href="https://www.instagram.com/"><img src={require('./img-footer/insta.png')} alt="" /></a>
+					<a href="https://twitter.com/?lang=ru"><img src={require('./img-footer/Twitter.png')} alt="" /></a>
+					<a href="https://www.linkedin.com/"><img src={require('./img-footer/Linkedin.png')} alt="" /></a>
+				</div>
+				<div>
+					<p><img src={require('./img-footer/Copyright ©2020 All rights reserved.png')} alt="" /></p>
+				</div>
+				
+			</div>
+		 </footer>
     </div>
   );
 }
