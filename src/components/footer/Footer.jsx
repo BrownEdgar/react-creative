28ab94329c4c0adb1cdeccc613bf008b3ce9b6d6
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-01 18:40:51 +0400

    change directory

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Footer.scss'
+
+export default function Footer() {
+	return (
+		<footer>
+			<div className='footer-img'>
+				<div className='footer-img-icon'>
+					<a href="https://ru-ru.facebook.com/"><img src={require('./img-footer/fb.png')} alt="" /></a>
+					<a href="https://www.instagram.com/"><img src={require('./img-footer/insta.png')} alt="" /></a>
+					<a href="https://twitter.com/?lang=ru"><img src={require('./img-footer/Twitter.png')} alt="" /></a>
+					<a href="https://www.linkedin.com/"><img src={require('./img-footer/Linkedin.png')} alt="" /></a>
+				</div>
+				<div>
+					<p><img src={require('./img-footer/Copyright ©2020 All rights reserved.png')} alt="" /></p>
+				</div>
+
+			</div>
+		</footer>
+	)
+}
