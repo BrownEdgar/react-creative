4d6a7548ec58b2d694f250ebf20bf37c114b2c13
raiskhachatryan <raiskhachatryan@gmail.com>
2023-06-01 17:19:42 +0400

    main-block (homepage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>React creative</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.scss";
+
+export default function Home() {
+  const handleDownload = () => {
+    console.log("downloaed");
+  };
+  return (
+    <div className="main">
+      <div>
+        <div className="left">
+          <h1>
+            Hi, I am John, <br />
+            Creative Technologist
+          </h1>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <div className="btn-download">
+          <button onClick={handleDownload}>Download Resume</button>
+        </div>
+      </div>
+      <div className="right">
+        <img className="person_img" alt="John" />{" "}
+      </div>
+    </div>
+  );
+}
