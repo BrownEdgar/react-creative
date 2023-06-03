7dea6806e22d3e1f40858f70ece31ecf6f9d2c3a
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-03 18:42:24 +0400

    added links constants

---
+++ src/components/constants/IconLinks.js
@@ -0,0 +1,7 @@
+const IconLinks = {
+	tw: 'https://twitter.com/?lang=ru',
+	fb: 'https://ru-ru.facebook.com/',
+	insta: 'https://www.instagram.com/',
+	linkedin: 'https://www.linkedin.com/',
+}
+export default IconLinks
